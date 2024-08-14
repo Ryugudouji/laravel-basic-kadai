30724c4a805294031b373369a67f3211c3dd8fbd
Ryugudouji <golden83caner@yahoo.co.jp>
2024-08-14 16:57:29 +0900

    laravel課題5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,12 +9,37 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+
+    // laravel課題5 ----------
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        //  フォームの入力内容をもとに、テーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+         // リダイレクトさせる
+        return redirect("/posts");
+
+    // -------------------------
+    }
+
     public function index() {
         // postsテーブルからすべてのデータを取得し、変数$postsに代入する
         $posts = DB::table('posts')->get();
 
         // 変数$postsをposts/index.blade.phpファイルに渡す
-        return view('posts/index', compact('posts'));
+        return view('posts.index', compact('posts'));
     }
 
     public function show($id) {
